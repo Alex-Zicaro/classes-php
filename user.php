55e195787623beb678c2fbfe15b0059c6f98fb0f
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-28 10:58:29 +0100

    user

---
+++ user.php
@@ -0,0 +1,21 @@
+<?php
+class User{
+    private $id;
+    public $login;
+    public $email;
+    public $firstname;
+    public $lastname;
+
+    public function __construct()
+    {
+        $this->id = $id;
+        $this->login = $login;
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+
+    public function register($login,)
+
+}
