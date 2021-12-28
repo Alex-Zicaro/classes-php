752efa7b88d547747e235739408a2f0137acda79
Alex-Zicaro <alex.zicaro@laplateforme.io>
2021-12-28 22:22:47 +0100

    bdd

--- user-pdo.php
+++ user-pdo.php
@@ -1,42 +1,48 @@
 <?php
 
-require_once("bdd.php");
-class User extends Connect{
+// accesseurs : getter (lire un attribut) getAttribute / setter (modifier un attribut) setAttribute
+
+class User 
+{
     private $_id;
     public $_login;
     public $_email;
     public $_firstname;
-    public $_lastname ;
-
-    public function __construct($id,$login,$email,$firstname,$lastname)
+    public $_lastname;
+    protected $dns = "mysql:host=localhost;";
+    protected $dbName = "dbname=classes;";
+    protected $dbCo = "charset=utf8', 'root', ''";
+    protected $bdd = new PDO($dns,$dbName,$dbCo);
+    public function __construct($id, $login, $email, $firstname, $lastname,$dns,$dbName,$dbCo)
     {
         $this->_id = $id;
         $this->_login = $login;
         $this->_email = $email;
         $this->_firstname = $firstname;
         $this->_lastname = $lastname;
+        $this->dns = $dns;
+        $this->dbName = $dbName;
+        $this->dbCo = $dbCo;
+        $bdd = new PDO($dns,$dbName,$dbCo);
     }
 
 
-    public function register($login,$password,$email,$firstname,$lastname){
-        
-        $sql = 
-        'INSERT INTO utilisateurs( login, password, email, firstname, lastname) 
+    public  function register($login, $password, $email, $firstname, $lastname)
+    {
+
+        $sql =
+            'INSERT INTO utilisateurs( login, password, email, firstname, lastname) 
         VALUE(
         '  . $_POST['login']  . $_POST["password"] . $_POST["email"] . $_POST["firstname"] . $_POST["lastname"] .  ') ';
 
-        $req = connect::$bdd->prepare($sql);
+        $req = $this->bdd->prepare($sql);
 
         $formLogin = htmlspecialchars(strip_tags($_POST["formLogin"]));
         $formPassword = $_POST["password"];
-        if(!empty($formLogin) && !empty($formPassword) && require("bdd.php")) {
-            $reqBddLogin = $bdd->prepare("SELECT * FROM utilisateurs WHERE login = :login");
+        if (!empty($formLogin) && !empty($formPassword)) {
+            $reqBddLogin = $this->bdd->prepare("SELECT * FROM utilisateurs WHERE login = :login");
         }
-
-
-
     }
-
 }
 
 // $test = new User();
